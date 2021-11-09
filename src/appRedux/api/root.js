2c7a1e7aa3964f31ec32f544db91d02bbd719277
chip <desdhi24@gmail.com>
2021-11-09 12:51:34 +0000

    wor

--- src/appRedux/api/root.js
+++ src/appRedux/api/root.js
@@ -1,12 +1,12 @@
 import axios from 'axios'
 
-export const BASE_URL = "https://chipsoft-pos.herokuapp.com/v1"
+export const BASE_URL = "https://api.riskprofiler.chipsoftgh.com/v1"
 export const ONLINE_TEST_URL = "https://risk-profiler.herokuapp.com/v1"
 export const TEST_URL = "http://localhost:8082/v1"
 
 const root = () => {
     return axios.create({
-        baseURL: TEST_URL,
+        baseURL: BASE_URL,
         withCredentials: true,
         headers: {
             'Access-Control-Allow-Origin': '*',
